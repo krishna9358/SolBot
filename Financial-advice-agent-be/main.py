566c4965b4e566834308388f2bf576d76b0f3b46
Krishna Mohan <krishanmohank974@gmail.com>
2025-05-22 01:38:15 +0530

    Update main.py

--- Financial-advice-agent-be/main.py
+++ Financial-advice-agent-be/main.py
@@ -21,13 +21,13 @@ from utils import *
 from consts import *
 from classes import *
 
-MODEL = "llama-3.1-8b-instant"
+MODEL = "llama-3.3-70b-versatile"
 # MODEL = "llama-3.3-70b-versatile"
 
 llm = ChatGroq(
     temperature=0,
     model_name=MODEL,
-    api_key="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",
+    api_key="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",
 )
 obb.obb.account.login(pat="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoX3Rva2VuIjoiUmc2aWFhdVExZ1dOcDUxWGlBUjRkYnVVUVllem5GR2t2S05EVWEybiIsImV4cCI6MTc2ODM4OTE0MX0.OdPqeDk8-U6b7XDyx42nClC3Mm_kmTwe77W1fQhksNU")
 obb.obb.user.preferences.output_type = "dataframe"
